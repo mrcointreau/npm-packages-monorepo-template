39396eafe45844577757c27db300b542da011892
Luca Martini <luca.g.martini@gmail.com>
2024-03-08 17:05:33 +0100

    chore: fix testing

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
+const { pathsToModuleNameMapper } = require('ts-jest')
+
+const { compilerOptions } = require('./tsconfig')
+
 module.exports = {
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   // See https://github.com/kulshekhar/ts-jest/issues/4081
   globals: {
     'ts-jest': {
--- packages/core/jest.config.cjs
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const config = require('../../jest.config.cjs')
-
-module.exports = {
-  ...config
-}
