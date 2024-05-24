653fbe768bc214083d4897813985fbaee92bbabd
Luca Martini <luca.g.martini@gmail.com>
2024-05-24 11:44:58 +0200

    chore: fix package2 vitest configuration

--- packages/package2/vitest.config.ts
+++ packages/package2/vitest.config.ts
@@ -1,6 +1,8 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   cacheDir: '../../node_modules/.vite',
   esbuild: {
     format: 'esm',
